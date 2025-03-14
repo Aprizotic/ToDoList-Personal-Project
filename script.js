2cf3d7497b24d68acb3a83595bc0442aa2aaeeb8
Kajus Butrimavicius <kajusbut24@gmail.com>
2025-03-14 17:31:47 +0000

    remove task button

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function addTask() {
-    let taskDetails = prompt('Enter Task Below');
+    let taskContent = prompt('Enter Task Below');
 
-    if (taskDetails) {
-        const newTask = document.createElement('li');
+    if (taskContent) {
+        const newList = document.createElement('li');
+        const newDiv = document.createElement('div');
+        const newContent = document.createElement('p');
+        const newRemoveButton = document.createElement('button');
         const tasks = document.getElementById('tasks');
 
-        newTask.classList.add('task');
-        newTask.textContent = taskDetails;
-        tasks.appendChild(newTask);
+        newRemoveButton.classList.add('remove-task');
+        newRemoveButton.textContent = '-';
+
+        newContent.classList.add('content');
+        newContent.textContent = taskContent;
+
+        newDiv.classList.add('task');
+        newDiv.appendChild(newContent);
+        newDiv.appendChild(newRemoveButton);
+
+        newList.appendChild(newDiv);
+
+        tasks.appendChild(newList);
+
+        newRemoveButton.addEventListener('click', function() {
+            newList.remove();
+        });
     }
     else {
         alert('Task cant be empty');
@@ -16,5 +33,9 @@ function addTask() {
 
 document.addEventListener('DOMContentLoaded', function() {
     const addTaskButton = document.getElementById('new-task');
+    const joke = document.getElementById('joke');
+    joke.addEventListener('click', function() {
+        alert('You have not completed this task yet');
+    })
     addTaskButton.addEventListener('click', addTask);
 });
